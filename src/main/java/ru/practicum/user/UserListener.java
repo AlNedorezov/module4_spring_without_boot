5e349311cc57696615e0aaaa4ec7209232ab0a72
Aleksandr Nedorezov <Al73rus@gmail.com>
2023-08-22 02:54:11 +0300

    Added logic for saving users during pulsar message handling

--- src/main/java/ru/practicum/user/UserListener.java
+++ src/main/java/ru/practicum/user/UserListener.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.api.Consumer;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageListener;
+import org.apache.pulsar.client.api.PulsarClientException;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,22 +13,31 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserListener implements MessageListener<UserDto> {
 
-    private static final String EVENT_NAME_MESSAGE_SUCCESSFULLY_SENT = "{} message successfully sent!";
-    private static final String KEY = "Key: {}";
-    private static final String MESSAGE_ID = "MessageId: {}";
-    private static final String EVENT_NAME_COLON_MESSAGE = "{} : {}";
-    private static final String MESSAGE_RECEIVED = "Message received. MessageId: {}, Key: {}, Value: {}, Properties: {}";
+    private final UserService userService;
+    private static final String MESSAGE_RECEIVED =
+            "{} message received. MessageId: {}, Key: {}, Value: {}, Properties: {}";
+
+    private static final String USER_SUCCESSFULLY_SAVED =
+            "User with id={} successfully saved!";
+
+    private static final String ENCOUNTERED_PROBLEMS_WHILE_SAVING_USER =
+            "Problems were encountered while saving the user!";
 
     @Override
     public void received(Consumer<UserDto> consumer, Message<UserDto> msg) {
         try {
-            // Do something with the message
+            final String eventName = UserDto.class.getSimpleName();
             log.info(MESSAGE_RECEIVED,
-                    msg.getMessageId(), msg.getKey(), new String(msg.getData()), msg.getProperties());
+                    eventName, msg.getMessageId(), msg.getKey(), new String(msg.getData()), msg.getProperties());
+
+            final UserDto savedUser = userService.saveUser(msg.getValue());
+
+            log.info(USER_SUCCESSFULLY_SAVED, savedUser.getId());
 
             // Acknowledge the message
             consumer.acknowledge(msg);
-        } catch (Exception e) {
+        } catch (PulsarClientException | NullPointerException e) {
+            log.info(ENCOUNTERED_PROBLEMS_WHILE_SAVING_USER);
             // Message failed to process, redeliver later
             consumer.negativeAcknowledge(msg);
         }
